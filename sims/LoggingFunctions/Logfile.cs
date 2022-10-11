e977a39b486f8e692ae0932bc1bf4193d2ca6f28
Anton <anton.homm@gmail.com>
2022-10-11 15:49:33 +0200

    Added logging function directory, access to wanted data in Datalanguages' controller is in prints. Next up: Make objects with it and put in a seperate database after figuring out the function

---
+++ sims/LoggingFunctions/Logfile.cs
@@ -0,0 +1,26 @@
+﻿namespace sims.LoggingFunctions
+{
+    public struct Logfile
+    {
+        public string Protocol = "";
+        public string Method = "";
+        public string Path = "";
+
+        public Logfile(string protocol, string method, string path)
+        {
+            Protocol = protocol;
+            Method = method;
+            Path = path;
+        }
+    }
+    public class LogSaver
+    {
+        Logfile log;
+
+        public void SavetoDB()
+        {
+            Console.WriteLine("Test");
+        }
+
+    }
+}
--- sims/Program.cs
+++ sims/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
 using Sims.Data;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,20 +19,23 @@ builder.Services.AddDbContext<SimsContext>(
             Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
     });
 
-// Filtering for logger, piping together what we want to log. 
+//REAL LOGGING DONE IN CONTROLLERS, piping together what we want to log.
 builder.Services.AddHttpLogging(options =>
 {
     //options.LoggingFields = HttpLoggingFields.All; //Dangerous, will log bodies;
     options.LoggingFields = HttpLoggingFields.RequestPath |      // Ex: /api/dataformats
                             HttpLoggingFields.RequestProtocol |  // Ex: HTTP/2
                             HttpLoggingFields.RequestMethod |    // Ex: GET
-                            HttpLoggingFields.ResponseStatusCode;// Ex: 200
+                            HttpLoggingFields.ResponseStatusCode | // Ex: 200                                                            
+                            HttpLoggingFields.ResponsePropertiesAndHeaders; // Ex Content-Type: application/json; charset=utf-8 Date: Tue, 11 Oct 2022 12:19:26 GMT
+                                                                            
 });
 
 var app = builder.Build();
 
-//Logging calls to the API, so called "Middleware" (Might just end up logging to console and inpractial 
+//THIS IS JUST FOR DEBUGGING
 app.UseHttpLogging();
+
 // Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
